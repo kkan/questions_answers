9daa83527248bff8683fbdb71d7f096eb869efc3
K. Kanyuka <kkan.web@gmail.com>
2016-09-28 18:39:06 +0300

    Devise. Acceptance test for sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :questions do
     resources :answers, only: [:create]
     get :answers, to: 'questions#show', on: :member
   end
+
+  root 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
