dffcff38c62a89ce4fb3da7c524f69491aa14515
K. Kanyuka <kkan.web@gmail.com>
2016-10-05 13:59:01 +0300

    Only authentificated users can create questions & answers.

--- spec/acceptance/answers/create_spec.rb
+++ spec/acceptance/answers/create_spec.rb
@@ -6,11 +6,11 @@ feature 'Answer the question', %q{
   I want to be able answer the question
 } do
 
-  before do
-    visit question_path(create(:question))
-  end
+  given(:user) { create(:user) }
 
   scenario do
+    sign_in(user)
+    visit question_path(create(:question))
     attrs = attributes_for(:answer)
     fill_in 'answer[body]', with: attrs[:body]
     click_on 'Create'
