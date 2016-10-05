32e9e5b590505df68d0de6227d920109a32d755a
K. Kanyuka <kkan.web@gmail.com>
2016-10-06 01:58:53 +0300

    Ability to delete answers. Some refactoring.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,15 +1,26 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!, only: [ :create ]
+  before_action :authenticate_user!, only: [ :create, :destroy ]
   
   def create
     @question = Question.find(params[:question_id])
-    @answer = Answer.new(answer_params.merge(question: @question))
+    @answer = Answer.new(answer_params.merge(question: @question, user: current_user))
     if @answer.save
       redirect_to @question
     else
       flash[:notice] = 'Error while saving answer.'
       render 'questions/show'
     end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    if @answer.user == current_user
+      @answer.destroy
+      flash[:notice] = 'Answer successfully destroyed'
+    else
+      flash[:alert] = 'You can\'t delete not yours answer'
+    end
+    redirect_back(fallback_location: question_path(@answer.question))
   end
 
   private
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Answer, type: :model do
   it { should belong_to(:question) }
   it { should validate_presence_of(:body) }
   it 'validates existing of question' do
-    expect(build(:answer)).to_not be_valid
+    expect(build(:answer, question: nil)).to_not be_valid
   end
 end
