324459142f5948199e4c13645f5c79a91bfad7ae
K. Kanyuka <kkan.web@gmail.com>
2016-10-10 01:44:05 +0300

    Questions controller refactoring. Views changes. Delete empty files. Acceptance tests refactoring. Assotiation tests for User.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many(:questions) }
+  it { should have_many(:answers) }
+
   it 'check if user author of question' do
     user = create(:user)
     question = create(:question, user: user)
