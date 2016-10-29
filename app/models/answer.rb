d70cb51124a6dfecd2349bf8b38f9634262190c3
K. Kanyuka <kkan.web@gmail.com>
2016-10-29 12:24:10 +0300

    Added field 'best' to answer. Validation of existing only one best answer for question, test for it.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,5 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+  validates :best, uniqueness: { scope: :question_id }, if: :best?
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Answer, type: :model do
   it 'validates existing of question' do
     expect(build(:answer, question: nil)).to_not be_valid
   end
+
+  it 'validates existing only one best answer for question' do
+    question = create(:question)
+    create_list(:answer, 3, question: question)
+    create(:answer, question: question, best: true)
+    expect(build(:answer, question: question, best: true)).to_not be_valid
+  end
 end
